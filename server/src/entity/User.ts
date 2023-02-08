a8df6ec7eee51c40fb4d066fb4c64ff4fbbafff7
Dani2294 <daniel.agbato@yahoo.fr>
2023-02-08 11:02:04 +0100

    added prettier config file, wip

--- server/src/entity/User.ts
+++ server/src/entity/User.ts
@@ -4,33 +4,33 @@ import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
 @Entity()
 @ObjectType()
 class User {
-	@Field()
-	@PrimaryGeneratedColumn()
-	id: number;
+  @Field()
+  @PrimaryGeneratedColumn()
+  id: number;
 
-	@Field()
-	@Column({ length: 150 })
-	firstName: string;
+  @Field()
+  @Column({ length: 150 })
+  firstName: string;
 
-	@Field()
-	@Column({ length: 150 })
-	lastName: string;
+  @Field()
+  @Column({ length: 150 })
+  lastName: string;
 
-	@Field()
-	@Column({ length: 200, unique: true })
-	email: string;
+  @Field()
+  @Column({ length: 200, unique: true })
+  email: string;
 
-	@Field()
-	@Column({ length: 255 })
-	password: string;
+  @Field()
+  @Column({ length: 255 })
+  password: string;
 
-	@Field()
-	@Column({ length: 155 })
-	role: string;
+  @Field()
+  @Column({ length: 155 })
+  role: string;
 
-	@Field()
-	@Column({ length: 155 })
-	subscriptionType: string;
+  @Field()
+  @Column({ length: 155 })
+  subscriptionType: string;
 }
 
 export default User;
--- server/src/index.ts
+++ server/src/index.ts
@@ -6,22 +6,22 @@ import { join } from "path";
 import db from "./db";
 
 async function start(): Promise<void> {
-	await db.initialize();
+  await db.initialize();
 
-	const schema = await buildSchema({
-		resolvers: [join(__dirname, "/resolvers/*.ts")],
-	});
+  const schema = await buildSchema({
+    resolvers: [join(__dirname, "/resolvers/*.ts")],
+  });
 
-	const server = new ApolloServer({
-		schema,
-		csrfPrevention: true,
-		cache: "bounded",
-		plugins: [ApolloServerPluginLandingPageLocalDefault({ embed: true })],
-	});
+  const server = new ApolloServer({
+    schema,
+    csrfPrevention: true,
+    cache: "bounded",
+    plugins: [ApolloServerPluginLandingPageLocalDefault({ embed: true })],
+  });
 
-	await server.listen().then(({ url }: { url: string }) => {
-		console.log(`🚀  Server ready at ${url}`);
-	});
+  await server.listen().then(({ url }: { url: string }) => {
+    console.log(`🚀  Server ready at ${url}`);
+  });
 }
 
 start().catch(console.error);
