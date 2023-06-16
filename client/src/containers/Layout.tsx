593dc24b38774d3f03d8be2772795c895cc359a6
Dani2294 <daniel.agbato@yahoo.fr>
2023-06-16 21:55:49 +0200

    updated home page challenge card

--- client/src/containers/Layout.tsx
+++ client/src/containers/Layout.tsx
@@ -9,7 +9,7 @@ function Layout() {
   return (
     <>
       <header className="sticky top-0 z-10 py-3 elevate-box rounded-none bg-background">
-        <div className="flex items-center justify-between container">
+        <div className="flex items-center justify-between">
           {/* LOGO */}
           <Button className="self-start rounded-full p-2 text-base">
             <Link to="/">GG</Link>
