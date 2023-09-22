d1e82afdf7c013e173f7e28298830c420f329f89
Samy <samy@sami.eco>
2023-09-22 10:41:59 +0200

    fix: use pluginenvironment to fix jest tests

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,10 +1,16 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import path from "path";
+import EnvironmentPlugin from "vite-plugin-environment";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react({ include: "**/*.tsx" })],
+  plugins: [
+    react({ include: "**/*.tsx" }),
+    EnvironmentPlugin({
+      VITE_PUBLIC_STRIPE_KEY: null,
+    }),
+  ],
   server: {
     watch: {
       usePolling: true,
