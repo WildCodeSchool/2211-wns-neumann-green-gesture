e796271f607d1ea8fb0332e3661e5fb9769fd959
Samy <samy@sami.eco>
2023-10-20 14:17:16 +0200

    remove containers script

--- deploy-staging.sh
+++ deploy-staging.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 git fetch origin && git reset --hard origin/dev && git clean -f -d && \
 docker compose -f docker-compose.staging.yml down && \
-docker rmi $(docker images -a -q) && \
+docker system prune -a && \
 docker compose -f docker-compose.staging.yml pull && \
 docker compose -f docker-compose.staging.yml --env-file .env.staging up -d;
